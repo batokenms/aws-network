6b1961c41dbcb92188a3e930e840590d8f22eca5
Jean <kenmogne12@gmail.com>
2022-12-11 21:51:02 -0600

    network code was modified

--- network.tf
+++ network.tf
@@ -1,9 +1,8 @@
-module "vpc" {
-  source                = "source4learn/vpc/aws"
-  version               = "0.1.1"
-  cluster_prefix        = "source4learn"
-  cluster_environment   = "development"
-  cluster_architecture  = "3-tier"
-  cidr                  = "10.0.0.0/20"
-  subnet_bits           = "4"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
